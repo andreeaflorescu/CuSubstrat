f240aaa33ae341313159f1e824195f1dd30227d4
Andreea Florescu <andreeaflorescu_15@yahoo.com>
2013-07-11 18:20:47 +0300

    read words from file

---
+++ src/Main.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+class Main {
+	private Trie trie;
+	private final String filename = "words.txt";
+	
+	Main() {
+		trie = new Trie();
+	}
+	//read words from file and add them to the Trie
+	public void createTrie() {
+		Scanner sc = new Scanner(filename);
+		while(sc.hasNext()) {
+			String word = sc.next();
+			trie.addWord(word);
+		}
+		sc.close();
+	}
+	
+	public static void main(String args[]) {
+//		new MainPanel();
+
+	}
+}
